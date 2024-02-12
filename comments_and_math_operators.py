722abaa5307fb438ce7c56ad58fb50761e67022b
Matheus de Lara Calache <matheuscalache@funda.nl>
2024-02-12 17:08:46 +0100

    Initial commit

---
+++ comments_and_math_operators.py
@@ -0,0 +1,54 @@
+# This is a comment
+
+# Basic Math Operators
+
+addition = 4 + 5            # 9
+subtraction = 5 - 2         # 3
+division = 7 / 2            # 3.5
+multiplication = 3 * 8      # 24
+
+print("addition: ", addition)
+print("subtraction: ", subtraction)
+print("division: ", division)
+print("multiplication: ", multiplication)
+
+# Exponentiation, Floor Division, and Modulo
+
+exponentiation = 4 ** 4         # 256
+floor_division = 16 // 5        # 3
+modulo = 7 % 3                  # 1
+
+print("exponentiation: ", exponentiation)
+print("floor_division: ", floor_division)
+print("modulo: ", modulo)
+
+# Assignment Operator
+
+add_assign = 5
+print("add_assign: ", add_assign)
+add_assign += 7         # 12
+print("add_assign: ", add_assign)
+
+sub_assign = 10
+sub_assign -= 5         # 5
+print("sub_assign: ", sub_assign)
+
+multi_assign = 10
+multi_assign *= 5            # 50
+print("multi_assign: ", multi_assign)
+
+div_assign = 10
+div_assign /= 5         # 2.0
+print("div_assign: ", div_assign)
+
+exp_assign = 10
+exp_assign **= 2        # 100
+print("exp_assign: ", exp_assign)
+
+floor_assign = 10
+floor_assign //= 6      # 1
+print("floor_assign: ", floor_assign)
+
+mod_assign = 10
+mod_assign %= 7         # 3
+print("mod_assign: ", mod_assign)
---
+++ variables.py
@@ -0,0 +1,43 @@
+ex_var = 5
+
+print("ex_var: ", ex_var)
+
+ex_var = 7
+
+print("ex_var: ", ex_var)
+
+float_1 = 1.2345
+float_2 = -1.25
+float_3 = 0.0
+float_4 = 13.0
+float_5 = -0.5
+
+print("float_1: ", float_1)
+print("float_2: ", float_2)
+print("float_3: ", float_3)
+print("float_4: ", float_4)
+print("float_5: ", float_5)
+
+int_1 = 7
+int_2 = -52
+int_3 = 0
+int_4 = 900
+int_5 = -749
+
+print("int_1: ", int_1)
+print("int_2: ", int_2)
+print("int_3: ", int_3)
+print("int_4: ", int_4)
+print("int_5: ", int_5)
+
+bool_1 = True
+bool_2 = False
+
+print("bool_1", bool_1)
+print("bool_2", bool_2)
+
+bool_1 = False
+bool_2 = True
+
+print("bool_1", bool_1)
+print("bool_2", bool_2)
