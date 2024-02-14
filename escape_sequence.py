b4d10365fe35e571806125f53e3e5028a11eb25e
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-14 12:59:49 +0100

    add escape sequences examples

---
+++ escape_sequence.py
@@ -0,0 +1,16 @@
+# Tab character \t
+
+print("This\tis\ta\tlot\tof\tspace.")
+
+# New line character \n
+
+print("line one\nline two")
+
+# Single quote and double quote characters \' and \"
+
+print('"When I said \'immediately,\' I meant today!", said King Saul.')
+print("\"Do or do not. There is no try.\"")
+
+# \\ for a back-slash character
+
+print("All escape sequences contain a \\.")
