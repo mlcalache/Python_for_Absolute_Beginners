73a78bd2d91d0ee3d1f82390fcd9cf519e318f16
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-20 17:44:33 +0100

    add input examples

---
+++ input.py
@@ -0,0 +1,14 @@
+name = input("Please enter your name:\n")
+print("You name is " + name + ".")
+print(type(name))
+
+fav_num = input("What is your favorite number?\n")
+print("Your favorite number is " + fav_num + ".")
+print(type(fav_num))
+
+exercise_name = input("What is your name?\n")
+exercise_quest = input("What is your quest?\n")
+exercise_color = input("What is your favorite color?\n")
+exercise_output = ("So your name is " + exercise_name + ", your quest is " + exercise_quest
+                   + ", and your favorite color is " + exercise_color + ".")
+print(exercise_output)
