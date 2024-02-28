74cb079def53c3df251adcb09bddf1c9dbbf54eb
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-28 16:22:57 +0100

    add for loops example, exercise, and challenge.

---
+++ for_loops.py
@@ -0,0 +1,24 @@
+print("example")
+
+word = "house"
+
+for letter in word:
+    print(letter)
+
+print("exercise")
+
+sentence = "hello world"
+
+for letter in sentence:
+    print(letter)
+
+print("challenge")
+
+user_input = input("enter any text")
+number_chars = 0
+
+for letter in user_input:
+    number_chars += 1
+
+print(user_input)
+print(number_chars)
