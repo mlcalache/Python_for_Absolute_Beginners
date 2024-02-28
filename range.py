ecdf73602bc9d8611f18162eaa31914dfd86f8b8
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-28 16:30:21 +0100

    add range examples.

---
+++ range.py
@@ -0,0 +1,27 @@
+one_argument = range(5)
+
+for num in one_argument:
+    print(num)
+
+print(type(one_argument))
+
+two_arguments = range(5,10)
+
+for num in two_arguments:
+    print(num)
+
+print(type(two_arguments))
+
+three_arguments = range(1, 20, 3)
+
+for num in three_arguments:
+    print(num)
+
+print(type(three_arguments))
+
+three_arguments_decrease = range(20, 1, -3)
+
+for num in three_arguments_decrease:
+    print(num)
+
+print(type(three_arguments_decrease))
