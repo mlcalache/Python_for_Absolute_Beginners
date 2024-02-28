f1e0c9c3c850aba830de993fa9999dc19f7b6b0c
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-28 15:49:44 +0100

    add truthy and falsy examples

---
+++ truthy_falsy_values.py
@@ -0,0 +1,14 @@
+#  integer values
+print(bool(0))  # False
+print(bool(1))  # True
+print(bool(40))  # True
+
+#  float values
+print(bool(0.0))  # False
+print(bool(1.0))  # True
+print(bool(40.0))  # True
+
+#  string values
+print(bool(""))  # False
+print(bool("Hello"))  # True
+print(bool("1"))  # True
