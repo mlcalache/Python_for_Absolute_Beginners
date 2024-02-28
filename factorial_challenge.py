c26239d24dc7f335e2497d2b4a58fd68fe2de494
Matheus de Lara Calache <mlcalache@gmail.com>
2024-02-28 16:43:13 +0100

    add factorial challenge.

--- factorial_challenge.py
+++ factorial_challenge.py
@@ -1,9 +1,24 @@
+def calculate_factorial(number):
+    if number == 0:
+        factorial = 1
+    else:
+        counter = number
+        factorial = 1
+
+        while counter > 0:
+            factorial *= counter
+            counter -= 1
+
+    print("factorial of " + str(number) + " is " + str(factorial))
+
+    return factorial
+
+
 user_input = int(input("enter a positive integer"))
-counter = user_input
-factorial = 1
+calculate_factorial(user_input)
 
-while counter > 0:
-    factorial *= counter
-    counter -= 1
-
-print("factorial of " + str(user_input) + " is " + str(factorial))
+calculate_factorial(0)
+calculate_factorial(1)
+calculate_factorial(3)
+calculate_factorial(4)
+calculate_factorial(5)
