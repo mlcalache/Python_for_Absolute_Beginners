ec64ae8b744ec3904ea4351eff7bd4ad6e7b04c5
Matheus de Lara Calache <mlcalache@gmail.com>
2024-03-07 13:40:42 +0100

    add additional prints to better understand the results after run.

--- range.py
+++ range.py
@@ -1,3 +1,4 @@
+print("one argument")
 one_argument = range(5)
 
 for num in one_argument:
@@ -5,6 +6,7 @@ for num in one_argument:
 
 print(type(one_argument))
 
+print("two arguments")
 two_arguments = range(5,10)
 
 for num in two_arguments:
@@ -12,6 +14,7 @@ for num in two_arguments:
 
 print(type(two_arguments))
 
+print("three arguments")
 three_arguments = range(1, 20, 3)
 
 for num in three_arguments:
@@ -19,6 +22,7 @@ for num in three_arguments:
 
 print(type(three_arguments))
 
+print("three arguments decrease")
 three_arguments_decrease = range(20, 1, -3)
 
 for num in three_arguments_decrease:
