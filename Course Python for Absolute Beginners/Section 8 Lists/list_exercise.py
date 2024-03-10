75a4ec43c2f3b970bf82ac453f6ae0a42397ef44
Matheus de Lara Calache <mlcalache@gmail.com>
2024-03-10 23:09:29 +0100

    Add list exercise.

---
+++ Course Python for Absolute Beginners/Section 8 Lists/list_exercise.py
@@ -0,0 +1,19 @@
+list_1 = [[0, 2], [4, 6], [8, 10], [12, 14]]
+var_1 = list_1[0]
+print(var_1)
+var_2 = list_1[-1][-1]
+print(var_2)
+
+list_2 = ["chair", "table", "desk", "lamp", "bed"]
+var_3 = list_2[-5]
+print(var_3)
+var_4 = "Most people own at least " + str(list_1[0][1]) + " " + var_3 + "s."
+print(var_4)
+
+list_3 = [0.98, 8.76, 6.54, 4.32]
+var_5 = list_3[1:]
+print(var_5)
+var_6 = list_3[1:3]
+print(var_6)
+var_7 = list_3[:2]
+print(var_7)
