d0d615f18cc26ba1ee6bd2eddaf250c754a7d754
Matheus de Lara Calache <mlcalache@gmail.com>
2024-03-26 14:52:24 +0100

    Add examples of list comprehensions.

--- Section 8 Lists/list_comprehensions.py
+++ Section 8 Lists/list_comprehensions.py
@@ -5,5 +5,7 @@ print(list_1)
 print(list_2)
 
 list_3 = [i * 2 for i in list_1 if i % 2 == 0]  # double the values of the list but only for the even ones
+list_4 = [i * 2 for i in list_1 if i % 2 == 1]  # double the values of the list but only for the odd ones
 
 print(list_3)
+print(list_4)
